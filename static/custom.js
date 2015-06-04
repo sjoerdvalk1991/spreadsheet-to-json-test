0295bc0a7894046228ded01a94d9dd298f8b9da4
Sjoerd Valk <sjoerd@ivaldi.nl>
2015-06-04 16:10:22 +0200

    fix the tweaks

--- static/custom.js
+++ static/custom.js
@@ -1,1 +1,17 @@
-$('.slider').slick();
+$( document ).ready(function() {
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider',
+		dots: true,
+		centerMode: true,
+		focusOnSelect: true
+	});
+
+});
