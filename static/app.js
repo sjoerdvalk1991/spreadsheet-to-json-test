7a055d882bfd795b1b8d7850aa95b8d934cc23e1
Sjoerd Valk <sjoerd@ivaldi.nl>
2015-06-04 14:16:16 +0200

    fix it for

--- static/app.js
+++ static/app.js
@@ -1,22 +1,21 @@
 var app = angular.module('plunker', []);
 
 app.controller('SuperCtrl', ['$scope', '$http', function($scope,$http) {
-    var spreadsheetID = '1L6C5jGuWvZ4L_oF6M46Sjk0X_jchp1bxwxwZ-MNg240';
+    var spreadsheetID = '1yjOuR_qKvU7cHgM2gJE7vHuHHgq1bRrNuOeczVjgjoY';
     var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
     var parse = function(entry) {
       console.log(entry);
-      var category = entry['gsx$category']['$t'];
-      var description = entry['gsx$description']['$t'];
-      var title = entry['gsx$title']['$t'];
+      var name = entry['gsx$naam']['$t'];
+      var stripes = entry['gsx$aantal']['$t']
+      var number = stripes.length;
       return {
-        category: category,
-        description: description,
-        title: title,
-        url: url
+        name: name,
+        number: number
       };
     }
     $http.get(url)
     .success(function(response) {
+      console.log(response);
       var entries = response['feed']['entry'];
       $scope.parsedEntries = [];
       for (key in entries) {
