a19315ce65bbf7f74700a3b1db49b06c0853f62c
Sjoerd Valk <sjoerd@ivaldi.nl>
2015-06-04 15:46:47 +0200

    fix slide

--- static/app.js
+++ static/app.js
@@ -4,7 +4,6 @@ app.controller('SuperCtrl', ['$scope', '$http', function($scope,$http) {
     var spreadsheetID = '1yjOuR_qKvU7cHgM2gJE7vHuHHgq1bRrNuOeczVjgjoY';
     var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
     var parse = function(entry) {
-      console.log(entry);
       var name = entry['gsx$naam']['$t'];
       var stripes = entry['gsx$aantal']['$t']
       var number = stripes.length;
@@ -15,7 +14,6 @@ app.controller('SuperCtrl', ['$scope', '$http', function($scope,$http) {
     }
     $http.get(url)
     .success(function(response) {
-      console.log(response);
       var entries = response['feed']['entry'];
       $scope.parsedEntries = [];
       for (key in entries) {
